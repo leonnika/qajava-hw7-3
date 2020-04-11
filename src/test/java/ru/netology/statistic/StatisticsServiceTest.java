8320c19abe27be22b09433afe94f49ad63627f8a
Ekaterina Rymareva <rymareva_ea@mail.ru>
2020-04-11 11:39:55 +0500

    change format @test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,15 +1,16 @@
 package ru.netology.statistic;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StatisticsServiceTest {
-       @Test
+    @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
-           long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 10};
-           long expected = 12;
-           long actual = service.findMax(incomesInBillions);
-           assertEquals(expected, actual);
-       }
-   }
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 10};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
+}
